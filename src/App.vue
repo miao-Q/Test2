dce05436963998abbd93a0cb42ccb208cb3e3a91
Miao <1725430830@qq.com>
2020-04-24 17:55:00 +0800

    app

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import jsonp from 'jsonp'
-// import axios from 'axios'
 export default {
   name: 'App',
   components: {
-    HelloWorld
   },
   data(){
-    return{
-      age :35,
-      data : ''
-    }
-    
 
   },
   mounted(){
-    let url ="/api/activity/servicetime";
-     jsonp(url,(err,res)=>{
-      let result = res;
-      this.data = result; 
-    })
   }
-    // ,mounted(){
-    //   axios.get(url).then(()=>{
-        
-    //   })
-    // }
 }
 </script>
 
